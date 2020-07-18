d8d8461e37299d0b3b609f908e8a07802ea59dfe
mkapadia009 <manan.a.kapadia@gmail.com>
2020-07-18 21:16:01 +0530

    Changes in all files

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let description = document.getElementById('description-item').value;
 let incomeType = document.getElementById('expensetype').value;
 let amount = document.getElementById('amount').value;
 let dispData = document.getElementById('balance');
+// document.querySelector('form-submit').addEventListener('submit', function(event){
+// event.preventDefault();
+// })
 
 let numAmount = parseInt(amount);
 
@@ -28,6 +31,8 @@ dispData.innerHTML = `Balance is: ${num.reduce(reducer)}`
 var row = ""
 row = `${row}<tr><td>${description}</td><td>${incomeType}</td><td>${amount}</td></tr>`;
 document.getElementById('table-list').innerHTML += row;
+
+
 
 }
 
